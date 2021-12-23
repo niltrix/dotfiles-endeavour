29c6acecfe844a2dcc1059ea857328978285a16d
nils <axlgilby@gmail.com>
2021-12-23 18:07:15 +0900

    Create dotfiles_install.sh

---
+++ dotfiles_install.sh
@@ -0,0 +1,14 @@
+git clone --bare https://github.com/niltrix/dotfiles-endeavour.git $HOME/.dotfiles
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
